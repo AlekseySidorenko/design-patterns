eca965a7ae6ba58f61ade5af43f4a49bfa6cb1c1
Aleksey Sidorenko <sidorenko.aleksey@gmail.com>
2019-02-05 20:35:03 +1000

    Chapter 3: Decorator

---
+++ src/main/java/ru/patterns/decorator/Whip.java
@@ -0,0 +1,27 @@
+package ru.patterns.decorator;
+
+public class Whip extends CondimentDecorator {
+    Beverage beverage;
+
+    public Whip(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Whip";
+    }
+
+    @Override
+    public double cost() {
+        double cost = beverage.cost();
+        if (beverage.getSize() == Size.TALL) {
+            cost += .10;
+        } else if (beverage.getSize() == Size.GRANDE) {
+            cost += .12;
+        } else if (beverage.getSize() == Size.VENTI) {
+            cost += .14;
+        }
+        return cost;
+    }
+}
